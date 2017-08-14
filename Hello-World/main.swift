c42cd990b80e6db006b9241345adcbb80de7215d
osnaldy <osnaldy@hotmail.com>
2017-08-14 17:08:06 -0400

    Initial Commit

---
+++ Hello-World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello-World
+//
+//  Created by Osnaldy Vasquez on 8/14/17.
+//  Copyright © 2017 Osnaldy Vasquez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
