d400ff96d8e3d91ef0bd38ff588087fa9c7bb318
osnaldy <osnaldy@hotmail.com>
2017-08-15 16:49:21 -0400

    basic Swift functionalities

--- Hello-World/main.swift
+++ Hello-World/main.swift
@@ -8,48 +8,71 @@
 
 import Foundation
 
-//let label = "The width is "
-//let width: Int = 94
-//let widthLabel = label + String(width)
-//print(widthLabel)
-//
+let vegetable = "red pepper"
+
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)")
+default:
+    print("Everything taste good on a soup")
+    
+}
+//---------------------------------------------------------------
+let label = "The width is "
+let width: Int = 94
+let widthLabel = label + String(width)
+print(widthLabel)
+
 //---------------------------------------------------------------
 
-//let apples = 3
-//let oranges = 5
-//let appleSummary = "I have \(apples) apples"
-//print(appleSummary)
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples"
+print(appleSummary)
 
-//var individualScores = [75, 43, 103, 87, 12]
-//
-//var teamScore = 0
-//
-//for score in individualScores
-//{
-//    if score > 50
-//    {
-//        teamScore += 3
-//    }
-//    else
-//    {
-//        teamScore += 1
-//    }
-//}
-//print(teamScore)
-//
+var individualScores = [75, 43, 103, 87, 12]
+
+var teamScore = 0
+
+for score in individualScores
+{
+    if score > 50
+    {
+        teamScore += 3
+    }
+    else
+    {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
 //---------------------------------------------------------------
-//var optionalStuff: String? = "HelloWorld"
-//print(optionalStuff == nil)
-//
-//var optionalName: String? = nil
-//
-//var greetings = "hello!"
-//
-//if let name = optionalName
-//{
-//    greetings = "Hello, \(name)"
-//}
-//else{
-//    greetings = "Hello, Osnaldy"
-//}
-//print(greetings)
+var optionalStuff: String? = "HelloWorld"
+print(optionalStuff == nil)
+
+var optionalName: String? = nil
+
+var greetings = "hello!"
+
+if let name = optionalName
+{
+    greetings = "Hello, \(name)"
+}
+else{
+    greetings = "Hello, Osnaldy"
+}
+print(greetings)
+//---------------------------------------------------------------
+
+
+
+
+
+
+
+
