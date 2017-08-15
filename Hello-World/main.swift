ca8b88b5cf951431a13c6920902ba181d27eba41
osnaldy <osnaldy@hotmail.com>
2017-08-15 16:32:30 -0400

    Basic Swift codes

--- Hello-World/main.swift
+++ Hello-World/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+//let label = "The width is "
+//let width: Int = 94
+//let widthLabel = label + String(width)
+//print(widthLabel)
+//
+//---------------------------------------------------------------
 
+//let apples = 3
+//let oranges = 5
+//let appleSummary = "I have \(apples) apples"
+//print(appleSummary)
+
+//var individualScores = [75, 43, 103, 87, 12]
+//
+//var teamScore = 0
+//
+//for score in individualScores
+//{
+//    if score > 50
+//    {
+//        teamScore += 3
+//    }
+//    else
+//    {
+//        teamScore += 1
+//    }
+//}
+//print(teamScore)
+//
+//---------------------------------------------------------------
+//var optionalStuff: String? = "HelloWorld"
+//print(optionalStuff == nil)
+//
+//var optionalName: String? = nil
+//
+//var greetings = "hello!"
+//
+//if let name = optionalName
+//{
+//    greetings = "Hello, \(name)"
+//}
+//else{
+//    greetings = "Hello, Osnaldy"
+//}
+//print(greetings)
